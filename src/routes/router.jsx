39aa480eb228f19a19e359622c2a3d7ade927498
Samanta <hernandezrios0101@gmail.com>
2020-11-23 12:08:24 -0500

    agregando router

---
+++ src/routes/router.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Switch, Route, Router} from "react-router-dom";
+
+
+function Router(){
+    return(
+        <BrowserRouter>
+        </BrowserRouter>
+    );
+}
+
+
+export default Router;
